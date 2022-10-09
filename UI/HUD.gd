1a5c75678b1535e26c36d320f676b1e1e3164164
NelsonAlemanMoreno <nalemanm.am.1107@gmail.com>
2022-10-08 23:17:18 -0400

    completed (2/2)

--- UI/HUD.gd
+++ UI/HUD.gd
@@ -1,10 +1,15 @@
 extends Control
 
+var lives_pos = Vector2.ZERO
+var lives_index = 30
+onready var Indicator = load("res://UI/Indicator.tscn")
+
 
 func _ready():
+	
 	update_score()
 	update_time()
-
+	update_lives()
 
 func update_score():
 	$Score.text = "Score: " + str(Global.score)
@@ -13,7 +18,13 @@ func update_time():
 	$Time.text = "Time: " + str(Global.time)
 
 func update_lives():
-	pass
+	lives_pos = Vector2(20, Global.VP.y - 20)
+	for child in $Indicator_Container.get_children():
+		child.queue_free()
+	for i in range(Global.lives):
+		var indicator = Indicator.instance()
+		indicator.position = Vector2(lives_pos.x + i*lives_index, lives_pos.y)
+		$Indicator_Container.add_child(indicator)
 	
 func _on_Timer_timeout():
 	Global.time -= 1
